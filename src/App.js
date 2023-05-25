bd60d120a60c3e5d601ab96e8a05d676a9be48d4
Саламат Кадыров <salamat@MacBook-Air-Salamat.local>
2023-05-25 14:13:46 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddForm from "./components/AddForm";
+import Details from "./components/Details";
+import EditForm from "./components/EditForm";
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
 
-function App() {
+const App = () => {
+  const API = "http://localhost:8000/products";
+
+  // ! CRUD - create, read, update, delete
+
+  // ! для хранения полученных продуктов
+  const [products, setProducts] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/add" element={<AddForm />} />
+        <Route path="/contacts" element={<h1>Contacts</h1>} />
+        <Route path="/edit/:id" element={<EditForm />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
