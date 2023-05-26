eb4827098e6e0be8858e37ff36a3ab910d5e5115
Саламат Кадыров <salamat@MacBook-Air-Salamat.local>
2023-05-26 12:13:32 +0600

    .

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,22 +1,29 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Spinner } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const Details = ({ getOneProduct, oneProduct }) => {
   const { id } = useParams();
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     getOneProduct(id);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   }, []);
+
+  const styles = { width: "200px" };
 
   return (
     <div className="container">
-      {oneProduct ? (
+      {!loading ? (
         <div>
-          <div>{oneProduct.title}</div>
-          <div>{oneProduct.price}</div>
-          <img src={oneProduct.image} alt="" />
+          <h2>{oneProduct.title}</h2>
+          <h4>{oneProduct.price}</h4>
+          <img src={oneProduct.image} alt="" style={styles} />
         </div>
       ) : (
         <Spinner animation="border" role="status">
